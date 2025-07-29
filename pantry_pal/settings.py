05e3280c1b1d7f9e485fb558d27d2c04d314cf59
Tima-S9 <fatimaadora@yahoo.fr>
2025-07-29 14:08:35 +0100

    key s updated and env.py updated

--- pantry_pal/settings.py
+++ pantry_pal/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!f@lz$l@za49h*%$qml*_kum0c(wse0nx=#e%g_248!%mg1&hc'
-
+# SECRET_KEY = 'django-insecure-!f@lz$l@za49h*%$qml*_kum0c(wse0nx=#e%g_248!%mg1&hc'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
